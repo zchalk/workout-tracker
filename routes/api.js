7b3411001c3496342e225f25bc81c2b4f6eed7c0
zchalk <you@example.com>
2021-06-11 21:14:58 -0400

    fixed broken routes, added last two, page routes non functional

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,40 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.get("/api/workouts", res => {
+router.get("/api/workouts", (req,res) => {
     Workout.aggregate([{
-        $addFields: {totalDuration: {$sum: "$exercises.duration"}}
-    }]).then(dbResults => {
-        res.json(dbResults);
+       $addFields: {totalDuration: {$sum: "$exercises.duration"}}
+    }]).then(dbWorkout => {
+        res.json(dbWorkout);
     }).catch(err => {
         res.status(400).json(err);
     });
 });
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([{
+        $addFields: {totalDuration: {$sum: "$exercises.duration"}}
+     }]).sort({ date: -1 }).limit(7)
+     .then(dbWorkout => {
+         res.json(dbWorkout);
+     }).catch( err => {
+         res.status(400).json(err)
+     });
+});
 router.put("/api/workouts/:id", ({body, params}, res) => {
     Workout.findOneAndUpdate(
         {_id: params.id}, {$push: {exercises: body}}, {new: true}
-        ).then(dbResults => {
-            res.json(dbResults);
+        ).then(dbWorkout => {
+            res.json(dbWorkout);
         }).catch(err => {
             res.status(400).json(err);
         });
 });
+router.post("/api/workouts", ({body}, res) => {
+    Workout.insert(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+module.exports = router;
