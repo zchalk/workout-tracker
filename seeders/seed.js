45831cb830d7ede82cb75d12d25af57c0e12f0ae
zchalk <you@example.com>
2021-06-15 19:15:40 -0400

    changed mongoose connect in seedjs

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const db = require('../models/workout.js');
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 const workoutSeed = [
